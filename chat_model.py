9cafcc75b457ac20d0561193beba4f0c0c66ab7e
SPFA <2997839760@qq.com>
2023-12-26 16:08:37 +0800

    加入 gpt3.5 接口

--- chat_model.py
+++ chat_model.py
@@ -1,27 +1,93 @@
 import requests
 import json
+import openai
+
+
+class ChatModel:
+    def chat(self):
+        raise NotImplementedError
+
+    def update_data(self, role, content):
+        raise NotImplementedError
+
 
 access_token = "24.16af52018cd524ca8fa5d3133dfd6c09.2592000.1705601666.282335-45332416"
 
 
-class Ernie:  # ERNIE-Bot 4.0
+class Ernie(ChatModel):  # ERNIE-Bot 4.0
     def __init__(self):
         self.url = f"https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token={access_token}"
         self.headers = {
             'Content-Type': 'application/json',
         }
+        self.data = {
+            'messages': [
+                {
+                    'role': 'user',
+                    'content': '你是谁？'
+                },
+                {
+                    'role': 'assistant',
+                    'content': '我是 Veronica，一位赛博管家。'
+                }
+            ]
+        }
 
-    def chat(self, data):
-        jsonData = json.dumps(data)
+    def chat(self):
+        jsonData = json.dumps(self.data)
         response = requests.request("POST", self.url, headers=self.headers, data=jsonData)
         result = response.json().get("result")
         return result
 
+    def update_data(self, role, content):
+        self.data["messages"].append({
+            'role': role,
+            'content': content
+        })
+
+
+class ChatGPT(ChatModel):
+    def __init__(self):
+        openai.api_key = "sk-oqJn98z22yZNRRoa2cDe012cF65849048f84D3BaBe6e1c69"
+        openai.api_base = "https://oneapi.xty.app/v1"
+        # self.client = OpenAI(
+        #     base_url="https://oneapi.xty.app/v1",
+        #     api_key="sk-oqJn98z22yZNRRoa2cDe012cF65849048f84D3BaBe6e1c69"
+        # )
+        self.data = []
+
+    def chat(self):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.data
+        )
+        result = response["choices"][0]["message"]["content"]
+        return result
+
+    def update_data(self, role, content):
+        self.data.append({
+            'role': role,
+            'content': content
+        })
+
+
+class ChatData:
+    def __init__(self, name, model="chatgpt"):
+        self.name = name
+        if model.lower() == "ernie": self.model = Ernie()
+        if model.lower() == "chatgpt": self.model = ChatGPT()
+
+    def gen_response(self, msg):
+        self.model.update_data("user", msg)
+        res = self.model.chat()
+        self.model.update_data('assistant', res)
+        return res
+
 
 if __name__ == '__main__':
-    chat = ChatModel()
+    chat = ChatData('yzc', "chatgpt")
     for i in range(3):
         ques = input(f"[{i}] You:\n\t")
-        chat.add_data('user', ques)
-        res, data = chat.chat()
+        res = chat.gen_response(ques)
         print(f"[{i}] Veronica:\n\t{res}")
+
--- wechat.py
+++ wechat.py
@@ -6,46 +6,12 @@ Date: 2023-12-20
 import itchat
 from itchat.content import TEXT
 import time
-from chat_model import Ernie
-
-
-class ChatData:
-    def __init__(self, name, model="ernie"):
-        self._name = name
-        self.data = {
-            'messages': [
-                {
-                    'role': 'user',
-                    'content': '你是谁？'
-                },
-                {
-                    'role': 'assistant',
-                    'content': '我是 Veronica，一位赛博管家。'
-                }
-            ]
-        }
-        if model == "ernie": self.ernie = Ernie()
-
-    @property
-    def name(self):
-        return self._name
-
-    def update_data(self, role, content):
-        self.data["messages"].append({
-            'role': role,
-            'content': content
-        })
-
-    def gen_response(self):
-        res = self.ernie.chat(self.data)
-        self.update_data('assistant', res)
-        return res
+from chat_model import ChatData
 
 
 if __name__ == '__main__':
     itchat.auto_login(hotReload=True)  # hotReload=True 可以暂存登录状态，退出后一定时间内重启不用再次扫码登录
     time.sleep(1)
-    chat = ChatModel()
     chatDict = {}
 
     @itchat.msg_register(TEXT)
@@ -55,21 +21,19 @@ if __name__ == '__main__':
         friendName = msg.User["UserName"]
         if friendName not in chatDict.keys():
             chatDict[friendName] = ChatData(friendName)
-        chatDict[friendName].update_data('user', msg.text)
-        res = chatDict[friendName].gen_response()
+        res = chatDict[friendName].gen_response(msg.text)
         print("send: ", res)
         itchat.send(res, toUserName=msg.User["UserName"])
 
 
-    @itchat.msg_register(TEXT, isGroupChat = True)
+    @itchat.msg_register(TEXT, isGroupChat=True)
     def group_reply(msg):
         if not msg['isAt']: return
         print(f"{msg.User['NickName']} receive: ", msg.text)
         groupName = msg.User["NickName"]
         if groupName not in chatDict.keys():
             chatDict[groupName] = ChatData(groupName)
-        chatDict[groupName].update_data('user', msg.text)
-        res = chatDict[groupName].gen_response()
+        res = chatDict[groupName].gen_response(msg.text)
         res = f"@{msg['ActualNickName']}\u2005 " + res
         print("send: ", res)
         itchat.send(res, toUserName=msg['FromUserName'])
