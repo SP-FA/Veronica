4aa0136eb42a894d3a45809df4be808f3b7b89da
SPFA <2997839760@qq.com>
2024-08-05 03:01:43 +0800

    add the relative path

--- timed_tasks.py
+++ timed_tasks.py
@@ -2,16 +2,16 @@ from utils.configure_util import ConfLoader
 from vero_auto_sign.get_params import get_user_params
 from vero_auto_sign.sign_request import sign_process
 
-import sys
-sys.path.append(".")
+import os
 
 if __name__ == "__main__":
-    path = "./conf.yaml"
+    rootPath = os.getcwd()
+    path = f"{rootPath}/conf.yaml"
     params = ConfLoader(path)
 
     # auto sign task
     users = ["SPFA"]
     for user in users:
-        signCfgPath = f"./vero_auto_sign/cfgs/{user}.cfg"
-        get_user_params(f"./vero_auto_sign/cfgs/{user}.cfg", "./vero_auto_sign/public_key.pem")
+        signCfgPath = f"{rootPath}/vero_auto_sign/cfgs/{user}.cfg"
+        get_user_params(f"{rootPath}/vero_auto_sign/cfgs/{user}.cfg", f"{rootPath}/vero_auto_sign/public_key.pem")
         sign_process(user, signCfgPath, params)
