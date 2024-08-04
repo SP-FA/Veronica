566889a67f564488c8f36d723a9c9ecc1adf4b16
SPFA <2997839760@qq.com>
2024-08-05 03:12:09 +0800

    add the relative path

--- timed_tasks.py
+++ timed_tasks.py
@@ -1,11 +1,10 @@
 from utils.configure_util import ConfLoader
 from vero_auto_sign.get_params import get_user_params
 from vero_auto_sign.sign_request import sign_process
-
-import os
+import sys
 
 if __name__ == "__main__":
-    rootPath = os.getcwd()
+    rootPath = sys.path[0]
     path = f"{rootPath}/conf.yaml"
     params = ConfLoader(path)
 
